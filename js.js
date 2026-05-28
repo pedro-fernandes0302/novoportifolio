b17bc14b68f7dd1ad8ca62cb20ece15da93124bf
pedro-fernandes0302 <pedrofernandesdejesus10@gmail.com>
2026-05-28 08:54:06 -0300

    update nos portifolios e habilidades

---
+++ js.js
@@ -0,0 +1,33 @@
+const stage = document.querySelector('.stage');
+
+stage.addEventListener('click', () => {
+  stage.classList.toggle('ativo');
+});
+
+//Efect scroll 
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+const serviceModals = document.querySelector(".service-modal");
+const learnmoreBtns = document.querySelector(".learn-more-btn");
+const modalCloseBtns = document.querySelector(".modal-close-btn");
+
+var modal = function (modalClick) {
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+    learnmoreBtn.addEventListener("click", () =>{
+        modal(i);
+    })
+})
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+         serviceModals.forEach((modalView) => {
+             modalView.classList.remove("active");
+         })
+    })
+})
